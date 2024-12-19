13090b5e421861faf5c947f96d42edef23bda89a
zidezhan <313040@s.lhvs.tyc.edu.tw>
2024-12-19 15:35:35 +0800

    Update 20241219建立類別.cpp

--- 20241219建立類別.cpp
+++ 20241219建立類別.cpp
@@ -11,3 +11,13 @@ void  mytime::prinTime(){
 	cout<<hour<<":"<<minute<<"\n";
 }
 int main(){
+mytime now, open;
+now.hour = 8;
+now.minute = 18;
+cout<<"現在時間:";
+now.prinTime();
+open.hour = 9;
+open.minute = 10
+cout<<"開發時間:";
+open.prinTime();
+}
