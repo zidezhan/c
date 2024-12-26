63adff0a983b1f7c2b65259b441fdb1b797a0c18
zidezhan <313040@s.lhvs.tyc.edu.tw>
2024-12-26 14:19:13 +0800

    Update 20241226類別成員.cpp

--- 20241226類別成員.cpp
+++ 20241226類別成員.cpp
@@ -12,9 +12,14 @@ class myTime{
 		int getMinute(){return minute;}
 		void printTime();
 };
+bool myTime::validTime(int h, int m){
+	if(h< 0|| h > 23)return false;
+	if(m <0|| m > 59)return false;
+	return true;
+}
 
-bool myTime::validTime(int h,int m){
-	if(valiTime(h,m)){
+bool myTime::setTime(int h,int m){
+	if(validTime(h,m)){
 		hour = h;
 		minute = m;
 		return true;
@@ -22,5 +27,21 @@ bool myTime::validTime(int h,int m){
 	else  return false;
 }
 void myTime::printTime(){
-	cont<<hour<<":"<<minute<<"\n";
+	cout<<hour<<":"<<minute<<"\n";
+}
+
+main(){
+	myTime open, close, now;
+	int h,m;
+	open.setTime(10,30);
+	close.setTime(17,30);
+	now.setTime(13,28);
+	cout<<"開張時間";
+	open.printTime();
+	cout<<"結束時間";
+	close.printTime();
+	cout<<"現在時間";
+	h = now.getHour();
+	m = now.getMinute();
+	cout<<h<<":"<<m<<"\n"; 
 }
